c085c26381a53fe14c6cb1c3c195995a7616ce68
rustaceanrob <rob.netzke@gmail.com>
2025-08-07 09:27:41 +0100

    Add `MessageRate` state machine

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,67 @@
+use std::time::Instant;
+
 /// Tools for validating messages and data
 pub mod validation;
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum MessageRate {
+    NoneReceived,
+    Ongoing { count: f64, start: Instant },
+}
+
+impl MessageRate {
+    fn new() -> Self {
+        Self::NoneReceived
+    }
+
+    fn add_single_message(&mut self, now: Instant) {
+        self.add_messages(1, now);
+    }
+
+    fn add_messages(&mut self, num_messages: usize, now: Instant) {
+        if num_messages == 0 {
+            return;
+        }
+        let num_messages = num_messages.try_into().unwrap_or(u32::MAX);
+        let num_message_float_repr = num_messages.into();
+        match self {
+            Self::NoneReceived => {
+                *self = Self::Ongoing {
+                    count: num_message_float_repr,
+                    start: now,
+                }
+            }
+            Self::Ongoing { count, start: _ } => *count += num_message_float_repr,
+        }
+    }
+
+    pub fn messages_per_secs(&self, now: Instant) -> Option<f64> {
+        match self {
+            Self::NoneReceived => None,
+            Self::Ongoing { count, start } => {
+                Some(*count / now.duration_since(*start).as_secs_f64())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::{Duration, Instant};
+
+    use crate::MessageRate;
+
+    #[test]
+    fn test_message_rate() {
+        let now = Instant::now();
+        let later = now.checked_add(Duration::from_secs(10)).unwrap();
+        let mut rate = MessageRate::new();
+        rate.add_messages(0, now);
+        assert!(matches!(rate, MessageRate::NoneReceived));
+        rate.add_single_message(now);
+        rate.add_messages(9, later);
+        assert_eq!(rate.messages_per_secs(later).unwrap(), 1.);
+        rate.add_messages(10, later);
+        assert_eq!(rate.messages_per_secs(later).unwrap(), 2.);
+    }
+}
